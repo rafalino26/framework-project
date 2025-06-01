5d9a1e25a752997300f457d4515921df6b9bcb3e
rafalino26 <rafax.lalujan@gmail.com>
2025-06-02 02:06:16 +0800

    feat: profile and logout integration

--- src/app/superadmindashboard/editor-profile/page.tsx
+++ src/app/superadmindashboard/editor-profile/page.tsx
@@ -5,7 +5,7 @@ export default function EditProfilePage() {
   return (
     <DashboardLayout>
       <div className="bg-white p-8 rounded-lg shadow-md max-w-4xl mx-auto">
-        <h1 className="text-2xl font-bold mb-6 text-gray-800">Edit Profile</h1>
+        <h2>tes</h2>
         <EditProfileContent />
       </div>
     </DashboardLayout>
